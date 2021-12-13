bdefe643aa49e85ed9e37260a428656aa9c6e0e7
Tan Bui <trungtanbui@gmail.com>
2021-12-13 18:24:53 +0200

    Add child React app

---
+++ child-react/webpack.config.js
@@ -0,0 +1,43 @@
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+module.exports = (options) => {
+  return {
+    entry: "./index.js",
+    output: {
+      filename: "bundle.js",
+      publicPath: "http://localhost:4204/",
+      uniqueName: "child-react",
+    },
+    module: {
+      rules: [
+        {
+          test: /.js$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: "babel-loader",
+              options: {
+                cacheDirectory: true,
+                presets: ["@babel/react", "@babel/env"],
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new ModuleFederationPlugin({
+        name: "childReact",
+        library: { type: "var", name: "childReact" },
+        filename: "remoteEntry.js",
+        exposes: {
+          "./web-components": "./app.js",
+        },
+        shared: ["react", "react-dom"],
+      }),
+    ],
+    devServer: {
+      port: 4204,
+    },
+  };
+};
